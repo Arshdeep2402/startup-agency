08b67d375073f1c29ea047437b136d4d8e28f608
Arshdeep2402 <arsh.nike12345@gmail.com>
2024-07-23 19:26:24 +0530

    about-us hero&feature section

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -2,37 +2,154 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
-import { curve, heroBackground, robot, check2, grid, loading1 } from '../../../public/assets'
+import { grid } from '../../../public/assets'
 import Button from '@/components/Button'
 import Section from "@/components/Section";
-import { BackgroundCircles, BottomLine,  } from '@/components/design/Hero'
-import { ScrollParallax } from "react-just-parallax";
-import { useRef } from "react";
-import Generating from '@/components/Generating'
-import CompanyLogos from '@/components/CompanyLogos'
+
 import Image from "next/image";
 
 import Heading from '@/components/Heading'
-import TagLine from '@/components/Tagline'
 import { intro } from '@/constants'
 import { Gradient } from '@/components/design/Roadmap'
 
+import Arrow from '../../../public/assets/svg/Arrow'
+import { GradientLight } from '@/components/design/Benefits'
+import ClipPath from '../../../public/assets/svg/ClipPath'
+import { benefits } from '@/constants'
+
+
 const page = () => {
-
-    // const parallaxRef = useRef(null);
-
+    
   return (
     <div>
         <Header />
 
+        <section className="container mx-auto px-5 md:px-10 h-full md:h-screen lg:pt-8 md:pt-24 sm:pt-32 pt-28">
+            <div className="flex items-center relative md:flex-row flex-col gap-10 md:gap-0">
+                <div className="flex-1 flex flex-col gap-5 sm:gap-8 md:gap-10 z-[1]">
+                <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-[#E6B9FF] to-[#9E89FF] capitalize text-5xl sm:text-6xl lg:text-7xl font-light">
+                    GenV Webseter, we are
+                </h1>
+                <p className="text-xs sm:text-sm font-[300] leading-loose">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo
+                    explicabo eius modi sunt quam nisi ea obcaecati beatae, iusto eaque
+                    dolorum, qui, molestias vitae laboriosam.
+                </p>
+                <button type="button" className="flex items-center -ml-7">
+                    <Image
+                    src="/play.png"
+                    width={80}
+                    height={80}
+                    alt="play button"
+                    className="mb-0 w-[80px] h-[50px] object-top object-cover"
+                    />
+                    <div className="text-left mt-2">
+                    <p className="font-semibold capitalize text-sm">
+                        let's get started!
+                    </p>
+                    <p className="text-xs mt-1">Watch quick demo video</p>
+                    </div>
+                </button>
+                </div>
+                <div className="flex-1">
+                    <Image
+                        src="/hero.png"
+                        width={450}
+                        height={450}
+                        alt="NFT"
+                        className="object-contain mx-auto max-w-[350px] lg:max-w-[450px]"
+                    />
+                </div>
+            </div>
+            <Image
+                src="/pink-gradients.png"
+                width={500}
+                height={500}
+                alt="gradients"
+                className="absolute top-0 left-0 -z-10"
+            />
+            <Image
+                src="/blue-gradients.png"
+                width={500}
+                height={500}
+                alt="gradients"
+                className="absolute top-0 left-0 -z-20"
+            />
+            <Image
+                src="/bg.png"
+                width={500}
+                height={500}
+                alt="gradients"
+                className="absolute top-0 left-0 -z-50 w-full h-[135%] xs:h-[130%] sm:h-[140%] md:h-screen"
+            />
+        </section>
 
-        
+        <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Discover All Features"
+        />
+
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h5 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <Image
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
+                </div>
+              </div>
+
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <Image
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
+            </div>
+          ))}
+        </div>
+      </div>
+        </Section>
+
         <Section  id="roadmap" className="pt-[14rem] -mt-[5.25rem]"
-      crosses
-      crossesOffset="lg:translate-y-[5.25rem]"
-      customPaddings>
+            crosses
+            crossesOffset="lg:translate-y-[5.25rem]"
+            customPaddings
+        >
             <div className="container md:pb-10">
-            <Heading tag="Our Team" title="What are We !" />
+            <Heading tag="Our Team" title="Who are We !" />
 
             <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
                 {intro.map((item) => {
@@ -96,8 +213,6 @@ const page = () => {
             </div>
             </div>
         </Section>
-
-
 
         <Footer />
     </div>
